c2bc3c0b73f3b422019f41c18b1c692043cfd84e
Juan Lopez <juanlopez4691@gmail.com>
2017-06-10 14:52:30 +0200

    Error alert: added fade effect animation

--- src/components/ErrorAlert.vue
+++ src/components/ErrorAlert.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="error alert alert-danger" v-if="errors && errors.length">
-    <ul>
-      <li v-for="error in errors">{{ error.message }}</li>
-    </ul>
-  </div>
+  <transition name="fade">
+    <div class="error alert alert-danger" v-if="errors && errors.length">
+      <ul>
+        <li v-for="error in errors">{{ error.message }}</li>
+      </ul>
+    </div>
+  </transition>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
 
 <script>
 export default {
