22cdd903553a087a8fa17f762b05ae8176649ca3
Juan Lopez <juanlopez4691@gmail.com>
2017-06-10 10:35:47 +0200

    Error alert component
    Created a Vue component to show error alerts.

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="error alert alert-danger" v-if="errors && errors.length">
+    <ul>
+      <li v-for="error in errors">{{ error.message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error-alert',
+  props: [ 'errors' ]
+}
+</script>
