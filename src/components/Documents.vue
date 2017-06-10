456980867ab7d51b303e47782c3d3d75ad4cd141
Juan Lopez <juanlopez4691@gmail.com>
2017-06-10 10:35:18 +0200

    Documents list component
    Created the Vue component to get a list
    of documents from the API and display it.

---
+++ src/components/Documents.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="documents-wrapper">
+    <error-alert :errors="errors"></error-alert>
+
+    <ul class="documents-list">
+      <li class="document" v-for="document in documents">
+        <a
+          href="#"
+          :data-id="document._id"
+          @click="displayDocument"
+          class="document-title"
+        >
+        {{ document.title }}
+        </a>
+        <a
+          href="#"
+          :data-id="document._id"
+          class="glyphicon glyphicon-remove-sign delete"
+          title="delete document"
+          @click="deleteDocument"
+        >
+        </a>
+        <br />
+        <span>{{ document.description }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .documents-list {
+    list-style: none;
+    padding: 0;
+    margin-left: 0;
+  }
+
+  .document {
+    margin-bottom: 1em;
+  }
+
+  .document-title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 1.5;
+    margin-bottom: .5em;
+
+    &:hover,
+    &:active,
+    &:focus,
+    &:visited {
+      text-decoration: none;
+    }
+  }
+
+  .delete {
+    font-size: 1.5em;
+    color: darkred;
+    vertical-align: text-bottom;
+
+    &:active,
+    &:focus,
+    &:visited {
+      color: darkred;
+      text-decoration: none;
+    }
+
+    &:hover {
+      text-decoration: none;
+      color: red;
+    }
+  }
+</style>
+
+<script>
+// Get rid of trailing slash in URL
+const trimUrl = url => url.replace(/\/$/, '')
+
+export default {
+  created () {
+    this.getDocumentsList()
+    this.$eventBus.$on('getDocumentsList', this.getDocumentsList)
+  },
+  props: [ 'api' ],
+  data () {
+    return {
+      documents: [
+      ],
+      errors: []
+    }
+  },
+  methods: {
+    displayDocument (event) {
+      const documentId = event.target.getAttribute('data-id')
+
+      this.axios.get(trimUrl(this.api) + '/' + documentId)
+      .then(response => {
+        console.dir(response.data)
+      })
+      .catch(e => {
+        this.errors.push(e.response.data)
+      })
+    },
+    deleteDocument () {
+      const documentId = event.target.getAttribute('data-id')
+
+      this.axios.delete(trimUrl(this.api) + '/' + documentId)
+      .then(response => {
+        this.getDocumentsList()
+      })
+      .catch(e => {
+        this.errors.push(e.response.data)
+      })
+    },
+    getDocumentsList () {
+      this.axios.get(this.api)
+      .then(response => {
+        this.documents = response.data
+      })
+      .catch(e => {
+        this.errors.push(e.response.data)
+      })
+    }
+  }
+}
+</script>
